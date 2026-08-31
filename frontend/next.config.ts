75fe8e3c56f41b16cbc76415403dbb8605fbf82f
Pranav Lamkhade <pranavlamkhade21@gmail.com>
2026-08-31 20:36:24 +0530

    Configure GitHub Actions and next.config.ts for GitHub Pages deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? "/MnSAT" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
